72c4323f46d9250206ef49c37ac15d3c55cdd482
henrykorir <longkhery@gmail.com>
2023-03-31 12:07:00 -0700

    added printif formatted string with %c, %s, %% conversion

--- printf.c
+++ printf.c
@@ -48,7 +48,7 @@ int _printf(const char *format, ...)
 			default:
 				if (p != NULL || *p != '\0')
 				{
-					/*count = print_and_count(p - 1, count);*/
+					count = print_and_count(p - 1, count);
 					count = print_and_count(p, count);
 					break;
 				}
